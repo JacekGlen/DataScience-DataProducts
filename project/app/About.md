0216160497fa974206c494679968aebd186f8a6d
Jacek Glen <jacek.glen@gmail.com>
2016-04-11 23:39:06 +0100

    app

---
+++ project/app/About.md
@@ -0,0 +1,13 @@
+### Storm Data Viewer
+
+Data is base on the U.S. National Oceanic and Atmospheric Administration's (NOAA) storm database.
+
+Data can be obtained from [NOAA website](https://www.ncdc.noaa.gov/stormevents/ftp.jsp). Please refer to [database details](https://www.ncdc.noaa.gov/stormevents/details.jsp) for more information.
+
+Source code is available on the [GitHub](https://github.com/JacekGlen/DataScience-DataProducts).
+
+### Usage
+
+From the slider in the left pane select time period for which to show data. You can select any of the available types of events.
+
+Use tabs to switch between different views. You can view map, charts by years or raw data.
--- project/app/ui.R
+++ project/app/ui.R
@@ -1,33 +1,63 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
 # 
-#    http://shiny.rstudio.com/
+# http://www.rstudio.com/shiny/
 #
 
 library(shiny)
+library(rCharts)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+shinyUI(
+  navbarPage("Storm Data Viewer",
+             tabPanel("Plot",
+                      sidebarPanel(
+                        sliderInput("range", 
+                                    "Range:", 
+                                    min = 1950, 
+                                    max = 2011, 
+                                    value = c(2000, 2011)
+                                    ),
+                        uiOutput("evtypeControls"),
+                        actionButton(inputId = "select_all", label = "Select all"),
+                        actionButton(inputId = "clear_all", label = "None",)
+                      ),
+                      
+                      mainPanel(
+                        tabsetPanel(
+
+                          # Data by state
+                          tabPanel("Impact by state",
+                                   column(7,
+                                          plotOutput("populationImpactByState")
+                                   )
+                                   
+                          ),
+                          
+                          # Time series data
+                          tabPanel("Charts",
+                                   h4('Number of events by year', align = "center"),
+                                   showOutput("eventsByYear", "nvd3"),
+                                   h4('Population impact by year', align = "center"),
+                                   showOutput("populationImpact", "nvd3"),
+                                   h4('Economic impact by year', align = "center"),
+                                   showOutput("economicImpact", "nvd3")
+                          ),
+                          
+
+                          # Data 
+                          tabPanel("Raw Data",
+                                   dataTableOutput(outputId="table"),
+                                   downloadButton('downloadData', 'Download')
+                          )
+                        )
+                      )
+                      
+             ),
+             
+             tabPanel("Help",
+                      mainPanel(
+                        includeMarkdown("About.md")
+                      )
+             )
   )
-))
+)
